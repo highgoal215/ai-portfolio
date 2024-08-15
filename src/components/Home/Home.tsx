1fc4cd9423dd505227fc423b1fe60784aa429539
highgoal215 <highgoal215@gmail.com>
2024-08-15 18:08:24 -0400

    mobile responsive styling

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,12 +1,17 @@
 import AIAssistant from './AI/AIAssistant'
 import BriefInfo from './BriefInfo'
+import ModalLoadingAnimation from "../About/ModalLoadingAnimation";
 
 export default function Home() {
 
     return(
         <div className="hello-wrapper">
-            <BriefInfo name={"Your Name"} role={"Web Developer"}/>
-            <AIAssistant />
+            <ModalLoadingAnimation duration="0.5s" direction="loadingBTT">
+                <BriefInfo name={"Your Name"} role={"Web Developer"}/>
+            </ModalLoadingAnimation>
+            <ModalLoadingAnimation duration="0.5s" direction="loadingTTB">
+                <AIAssistant />
+            </ModalLoadingAnimation>
         </div>
     )
 }
