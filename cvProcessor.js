81bdc81a515c2e8bba87e9afc1d4e906a75e0963
highgoal215 <highgoal215@gmail.com>
2024-08-21 22:58:16 -0400

    remove backend dependencies

--- cvProcessor.js
+++ cvProcessor.js
@@ -1,13 +1,14 @@
 import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+import { createClient } from "@supabase/supabase-js"
+import { SupabaseVectorStore } from "@langchain/community/vectorstores/supabase";
+import { OpenAIEmbeddings } from '@langchain/openai'
 
 try {
     const loader = new PDFLoader("cv.pdf", {
         splitPages: false
     });
     const docs = await loader.load();
-    // console.log(docs)
-    console.log(docs[0].pageContent)
 
     const splitter = new RecursiveCharacterTextSplitter({
         chunkSize: 600,
@@ -15,7 +16,21 @@ try {
         chunkOverlap: 100
     })
     const output = await splitter.createDocuments([docs[0].pageContent])
-    console.log(output)
+    // console.log(output)
+
+    const sbApiKey = process.env.SUPABASE_API_KEY
+    const sbUrl = process.env.SUPABASE_URL_PROFILE_AGENT
+    const openAIApiKey = process.env.OPEN_AI_API_KEY
+
+    const client = createClient(sbUrl, sbApiKey)
+    await SupabaseVectorStore.fromDocuments(
+        output,
+        new OpenAIEmbeddings({openAIApiKey}),
+        {
+            client,
+            tableName: 'documents'
+        }
+    )
 
 } catch (error) {
     console.error(error)
